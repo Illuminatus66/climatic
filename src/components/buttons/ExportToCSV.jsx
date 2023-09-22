4c1fa7d26f24ec0f1e4b41ebabb764a20c5366a3
Illuminatus66 <varunsodhani123@gmail.com>
2023-09-22 12:54:32 +0530

    every other button except for ExportToCSV button is now fully functional. I initially used the btoa function to encode the data into a Base64 string to be passed to the download url but ChatGPT instead suggested using the Buffer.from() method. Turns out that doesn't work in a browser environment and only works inside of a Node.js runtime so I'm now trying to go with my initial thought

--- src/components/buttons/ExportToCSV.jsx
+++ src/components/buttons/ExportToCSV.jsx
@@ -6,56 +6,57 @@ function ExportToCSV() {
   const [isExporting, setIsExporting] = useState(false);
   const mongodata = useSelector((state) => state.mongo.data);
 
-  const downloadCSV = () => {
+  const downloadCSV = async () => {
     if (isExporting || !mongodata) {
       return;
     }
 
     setIsExporting(true);
 
-    try{
-    const rows = mongodata.reduce((acc, location) => {
-      location.weather.forEach((interval) => {
-        const row = {
-          'Date': interval.startTime,
-          'Cloud Cover': interval.values.cloudCover,
-          'Dew Point': interval.values.dewPoint,
-          'Humidity': interval.values.humidity,
-          'Precipitation Intensity': interval.values.precipitationIntensity,
-          'Precipitation Probability': interval.values.precipitationProbability,
-          'Pressure Sea Level': interval.values.pressureSeaLevel,
-          'Sunrise Time': interval.values.sunriseTime,
-          'Sunset Time': interval.values.sunsetTime,
-          'Temperature': interval.values.temperature,
-          'Temperature Apparent': interval.values.temperatureApparent,
-          'UV Index': interval.values.uvIndex,
-          'Visibility': interval.values.visibility,
-          'Weather Code Day': interval.values.weatherCodeDay,
-          'Weather Code Night': interval.values.weatherCodeNight,
-          'Wind Speed': interval.values.windSpeed,
-        };
-        acc.push(row);
+    try {
+      const rows = mongodata.reduce((acc, location) => {
+        location.weather.forEach((interval) => {
+          const row = {
+            'Date': interval.startTime,
+            'Cloud Cover': interval.values.cloudCover,
+            'Dew Point': interval.values.dewPoint,
+            'Humidity': interval.values.humidity,
+            'Precipitation Intensity': interval.values.precipitationIntensity,
+            'Precipitation Probability': interval.values.precipitationProbability,
+            'Pressure Sea Level': interval.values.pressureSeaLevel,
+            'Sunrise Time': interval.values.sunriseTime,
+            'Sunset Time': interval.values.sunsetTime,
+            'Temperature': interval.values.temperature,
+            'Temperature Apparent': interval.values.temperatureApparent,
+            'UV Index': interval.values.uvIndex,
+            'Visibility': interval.values.visibility,
+            'Weather Code Day': interval.values.weatherCodeDay,
+            'Weather Code Night': interval.values.weatherCodeNight,
+            'Wind Speed': interval.values.windSpeed,
+          };
+          acc.push(row);
+        });
+        return acc;
+      }, []);
+
+      const csv = Papa.unparse(rows);
+
+      const base64Data = btoa(csv);
+
+      const dataUri = `data:text/csv;base64,${base64Data}`;
+      const a = document.createElement('a');
+      a.href = dataUri;
+      a.download = 'weather_data.csv';
+
+      a.addEventListener('click', () => {
+        setIsExporting(false);
       });
-      return acc;
-    }, []);
 
-    const csv = Papa.unparse(rows);
-
-    const base64Data = Buffer.from(csv).toString('base64');
-    const dataUrl = `data:text/csv;base64,${base64Data}`;
-    const a = document.createElement('a');
-    a.href = dataUrl;
-    a.download = 'weather_data.csv';
-
-    a.addEventListener('click', () => {
+      a.click();
+    } catch (error) {
+      console.error('Error generating CSV:', error);
       setIsExporting(false);
-    });
-
-    a.click();
-  } catch (error) {
-    console.error('Error generating CSV:', error);
-    setIsExporting(false);
-  }
+    }
   };
 
   return (
