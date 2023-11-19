b35727ba92e49ee7f0b1c5a9f622050857d9bc77
Illuminatus66 <varunsodhani123@gmail.com>
2023-11-19 23:12:11 +0530

    Now that I've decided that I don't want any animations for the individual weather entries inside the locations list

--- src/components/visualize/WeatherDataList/LocationsList.jsx
+++ src/components/visualize/WeatherDataList/LocationsList.jsx
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect, createRef } from "react";
 import { animate, motion, AnimatePresence, inView } from "framer-motion";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import WeatherEntry from "./WeatherEntry";
 
 const LocationsList = ({ data, selectedEntries, onSelect, dateRange, setDateRange }) => {
   const [expandedId, setExpandedId] = useState(null);
@@ -11,10 +10,16 @@ const LocationsList = ({ data, selectedEntries, onSelect, dateRange, setDateRang
   const itemRefs = useRef(new Map());
 
   useEffect(() => {
+    const newRefs = new Map();
+  
     data.forEach((item) => {
-      const ref = itemRefs.current.get(item.location.place) || createRef();
-      itemRefs.current.set(item.location.place, ref);
+      const key = `${item.location.place}-${item.createdAt}`;
+      newRefs.set(key, itemRefs.current.get(key) || createRef());
+    });
 
+    itemRefs.current = newRefs;
+
+    itemRefs.current.forEach((ref) => {
       if (ref.current) {
         inView(ref.current, {
           root: listRef.current,
@@ -29,6 +34,7 @@ const LocationsList = ({ data, selectedEntries, onSelect, dateRange, setDateRang
       }
     });
   }, [data]);
+  
 
   const handleSelect = (entryId) => {
     onSelect((prevSelectedEntries) => {
@@ -57,7 +63,7 @@ const LocationsList = ({ data, selectedEntries, onSelect, dateRange, setDateRang
   return (
     <div
       className="weather-data-list"
-      style={{ overflowY: "scroll", height: "100vh", backgroundColor: "#e6ccff" }}
+      style={{ overflowY: "scroll", height: "101vh", backgroundColor: "#e6ccff" }}
       ref={listRef}
     >
       <div 
@@ -78,22 +84,22 @@ const LocationsList = ({ data, selectedEntries, onSelect, dateRange, setDateRang
         </button>
       </div>
       {sortedData.map((locationData) => (
-        <div key={locationData.location.place} className="location-section">
-          <h3 onClick={() => toggleExpand(locationData.location.place)}
+        <div key={`${locationData.location.place}-${locationData.createdAt}`} className="location-section">
+          <h3 onClick={() => toggleExpand(`${locationData.location.place}-${locationData.createdAt}`)}
             style={{
               backgroundColor: '#d9b3ff',
               cursor: 'pointer',
               padding: '10px',
               margin: '10px 0',
-              boxShadow: expandedId === locationData.location.place ? 'none' : '0px 4px 8px rgba(0, 0, 0, 0.1)',
+              boxShadow: expandedId === `${locationData.location.place}-${locationData.createdAt}` ? 'none' : '0px 4px 8px rgba(0, 0, 0, 0.1)',
               borderRadius: '5px',
             }}
-            ref={itemRefs.current.get(locationData.location.place)}
+            ref={itemRefs.current.get(`${locationData.location.place}-${locationData.createdAt}`)}
           >
             {locationData.location.place}
           </h3>
           <AnimatePresence>
-            {expandedId === locationData.location.place && (
+            {expandedId === (`${locationData.location.place}-${locationData.createdAt}`) && (
               <motion.ul
                 initial={{ opacity: 0, height: 0 }}
                 animate={{ opacity: 1, y: 0 }}
@@ -108,12 +114,42 @@ const LocationsList = ({ data, selectedEntries, onSelect, dateRange, setDateRang
                 }}
               >
                 {locationData.weather.map((weatherEntry) => (
-                  <WeatherEntry
-                    key={weatherEntry._id}
-                    weatherEntry={weatherEntry}
-                    selectedEntries={selectedEntries}
-                    handleSelect={handleSelect}
-                  />
+                  <motion.li
+                  className="weather-item"
+                  onClick={() => handleSelect(weatherEntry._id)}
+                  style={{
+                    border: selectedEntries.includes(weatherEntry._id) ? '2px solid lightblue' : 'none',
+                    cursor: 'pointer',
+                  }}
+                >
+                  <div className="date">
+                    {new Date(weatherEntry.startTime).toLocaleDateString()}
+                  </div>
+                  <div>ID: {weatherEntry._id}</div>
+                  <div>Temperature: {weatherEntry.values.temperature}°C</div>
+                  <div>
+                    Temperature Apparent: {weatherEntry.values.temperatureApparent}°C
+                  </div>
+                  <div>Cloud Cover: {weatherEntry.values.cloudCover}%</div>
+                  <div>Dew Point: {weatherEntry.values.dewPoint}°C</div>
+                  <div>Humidity: {weatherEntry.values.humidity}%</div>
+                  <div>
+                    Precipitation Intensity: {weatherEntry.values.precipitationIntensity}
+                    &nbsp;mm/hr
+                  </div>
+                  <div>
+                    Precipitation Probability:{" "}
+                    {weatherEntry.values.precipitationProbability}%
+                  </div>
+                  <div>PSL: {weatherEntry.values.pressureSeaLevel}&nbsp;hPa</div>
+                  <div>Sunrise Time: {weatherEntry.values.sunriseTime}</div>
+                  <div>Sunset Time: {weatherEntry.values.sunsetTime}</div>
+                  <div>UV Index: {weatherEntry.values.uvIndex}</div>
+                  <div>Visibility: {weatherEntry.values.visibility}km</div>
+                  <div>Weather for Day: {weatherEntry.values.weatherCodeDay}</div>
+                  <div>Weather for Night: {weatherEntry.values.weatherCodeNight}</div>
+                  <div>Wind Speed: {weatherEntry.values.windSpeed}m/s</div>
+                </motion.li>
                 ))}
               </motion.ul>
             )}
